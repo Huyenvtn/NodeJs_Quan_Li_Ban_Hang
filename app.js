88bd7f1558e95ee0bd764c31041003b59727e098
Huyenvtn <huyenvtn0210@gmail.com>
2019-07-06 05:56:27 +0700

    9 commit

--- app.js
+++ app.js
@@ -9,72 +9,152 @@ const bodyParser = require('body-parser');
 var url = require('url');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+var session = require('express-session');
+app.use(session({ secret: "Nhỏ thôi bí mật!" }));
+
+ExportSP = async function (req, res){
+  var q = url.parse(req.url, true).query;
+  
+    if (q.muasp != undefined){
+        if (req.session.giohang == undefined)
+        {
+            req.session.giohang = [];
+            var sp = {
+                masp: q.muasp, hinhsp: q.hinh, tensp: q.tensp,
+                dongia: q.dongia, soluong: 1
+            };
+            req.session.giohang[0] = sp;   
+        }
+        else
+        {
+            var damua = 0; 
+            for (i = 0; i < req.session.giohang.length; i++)
+            {
+                if (req.session.giohang[i].masp == q.muasp) 
+                {
+                    req.session.giohang[i].soluong++;
+                    damua = 1;
+                    break;
+                }
+            }
+            if (damua == 0) {
+                var sp = {
+                  masp: q.muasp, hinhsp: q.hinh, tensp: q.tensp,
+                  dongia: q.dongia, soluong: 1
+                };
+                req.session.giohang[req.session.giohang.length] = sp;
+            }
+        }
+    }
+
+    var tenkh = "<a href=/?dangnhap=1>Đăng nhập</a>";
+    var thoat = "<a href=/?dangky=1>Đăng ký</a>";
+    if (req.session.kh == "") {
+        tenkh = "Thông tin dang nhap sai";
+        req.session.kh = undefined;
+    }
+    else if (req.session.kh != undefined) {
+        tenkh = req.session.kh.TENKH;
+        thoat = "<a href=/?thoat=1>Thoát</a>";
+    }
+    var xgh = "";
+    if (req.session.giohang != undefined && req.session.giohang.length > 0)
+        xgh = csdl.HienThiGioHang(req.session.giohang);
+    if (q.thanhtoan == 1 || q.muasp != undefined){
+          res.render("thanhtoan", {
+            giohang: xgh,
+            hoten: tenkh, thoat: thoat
+        });
+        }else if (q.sanpham != undefined) {
+            if (q.maloai != undefined) {
+                var  maloai = q.maloai;
+                dssp = await csdl.HienThiSP(maloai);
+                res.render("sanpham",{
+                    dssp: dssp,
+                    hoten: tenkh, thoat: thoat
+                })
+            }
+            else if (q.khoanggia != undefined) {
+                var  khoanggia = q.khoanggia;
+                dssp = await csdl.PhanLoaiGiaSP(khoanggia);
+                res.render("sanpham",{
+                    dssp: dssp,
+                    hoten: tenkh, thoat: thoat
+                })
+            }
+            else if (q.tensp != undefined) {
+                var tensp = q.tensp;
+               var dssp = await csdl.HienThiSPTen(tensp);
+                res.render("sanpham",{
+                    dssp: dssp,
+                    hoten: tenkh, thoat: thoat
+                })
+            } else if (q.masp != undefined){
+                var  masp = q.masp;
+                    sp = await csdl.ChiTietSP(masp);
+                    res.render("sanpham",{
+                        dssp: sp,
+                        hoten: tenkh, thoat: thoat
+                    })
+            }
+            else{
+                    var  maloai = 0;
+                    dssp = await csdl.HienThiSP(maloai);
+                    res.render("sanpham",{
+                        dssp: dssp,
+                        hoten: tenkh, thoat: thoat
+                    })
+            }
+        } else {
+    
+    if (q.lienhe == 1)
+        res.render("lienhe", {
+            hoten: tenkh, thoat: thoat
+        });
+    else if (q.baiviet == 1){
+        res.render("camnang", {
+            hoten: tenkh, thoat: thoat
+        });
+    } else{
+        res.render("index", {
+            hoten: tenkh, thoat: thoat
+        });
+    }
+}
+};
 
 app.get("/", async function (req, res) {
   var q = url.parse(req.url, true).query;
-  if (q.dangky == 1) {       
+  if (q.dangky == 1) {
       res.render("dangky");
-  }else if (q.lienhe ==1){res.render("lienhe");}
-  else if (q.sanpham == 1){
-    if (q.maloai != undefined){
-      var  maloai = q.maloai;
-        dssp = await csdl.HienThiSP(maloai);
-      res.render("sanpham", {headline: 'Đăng nhập', dssp:dssp})
-    }else
-      if (q.masp != undefined){
-        var masp = q.masp;
-        sp = await csdl.ChiTietSP(masp);
-        res.render("sanpham",{headline:'Đăng nhập', dssp:sp})
-      } else{
-     var maloai = 0;
-      dssp = await csdl.HienThiSP(maloai);  
-      res.render("sanpham", {headline: 'Đăng nhập', dssp:dssp})}
-  }
-  else if (q.thanhtoan == 1){
-    res.render("thanhtoan")
-  } else if(q.dangnhap == 1){
-      res.render("dangnhap")
-  } else if (q.baiviet == 1){
-    res.render("camnang", {headline: 'Đăng nhập'})
-  }else
-    {
-      res.render("index");
-  }   
-});
-app.get ("/index", function (req, res){
-  res.render("index");
+  } else if (q.dangnhap == 1){
+    res.render("dangnhap");
+    }else { 
+        if (q.thoat == 1)
+            req.session.kh = undefined;
+        await ExportSP(req, res);
+    }
 });
 
-app.post("/dangnhapthanhcong", async function (req, res) {
-  var emaildn = req.body.email;
+app.post("/dangnhap", async function (req, res) {
+  var email = req.body.email;
   var matkhau = req.body.matkhau;
   var kq = await csdl.DangNhap(email, matkhau);
   if (kq == 0) 
       req.session.kh = "";
   else
       req.session.kh = kq;
-  await XuatDuLieu(req, res);
+        await ExportSP(req, res);
 });
 
-app.post("/dangkythanhcong", async function (req, res) {
+app.post("/dangky", async function (req, res) {
   var tenkh = req.body.ten_kh;
   var email = req.body.email_kh;
   var sdt = req.body.sdt_kh;
   var matkhau = req.body.mk_kh;
-  console.log(tenkh);
-  console.log(sdt);
   var kq = await csdl.Dang_ky(tenkh, email, sdt, matkhau);
-  var maloai = 0;
-  var dssp = await csdl.HienThiSP(maloai);
-  res.render("sanpham", {headline: 'Chào '+ tenkh, dssp: dssp});
+        await ExportSP(req, res);
 });
+
 var server = http.createServer(app);
 server.listen(8000);
-
-//module.exports = app;
-
-//app.set('port', (8080));
-// DataBase 
-//XuatDuLieu = async function (req, res){};
-
-  //await XuatDuLieu(req, res);
--- routes/xulicsdl.js
+++ routes/xulicsdl.js
@@ -13,13 +13,10 @@ module.exports.Dang_ky =
         var kq;
         var caulenh = "insert into khachhang(TENKH, EMAIL,";
         caulenh+="SDT, MATKHAU) "
-        caulenh += "values('"+ TENKH + "','"
-            + EMAIL;
+        caulenh += "values('"+ TENKH + "','" + EMAIL;
         caulenh += "','" + SDT + "','" + MATKHAU + "')";
-        console.log(caulenh);
-       kq = await conn.query(caulenh);
-        console.log(kq);
-    return kq;
+        kq = await conn.query(caulenh);
+        return kq;
 };
 
 module.exports.DangNhap = async function (email, matkhau) {
@@ -32,11 +29,11 @@ module.exports.DangNhap = async function (email, matkhau) {
         connectionLimit: 10,
         queueLimit: 0
     });
-    var dskh;
-    dskh = await pool.query("select * from khachhang where EMAIL='"
+    var kh;
+    kh = await pool.query("select * from khachhang where EMAIL='"
         + email + "' and MATKHAU='" + matkhau + "'");
     var kq;
-    BangKh = dskh[0];
+    BangKh = kh[0];
     if (BangKh.length > 0) {
         kq = BangKh[0];
     }
@@ -45,67 +42,76 @@ module.exports.DangNhap = async function (email, matkhau) {
     return kq;
 };
 
-module.exports.PhanLoaiSP = async function () {
+module.exports.PhanLoaiGiaSP = async function (khoanggia) {
     var mysql = require('mysql2/promise');
     var pool = mysql.createPool({
         host: 'localhost',
         user: 'root',
         database: 'quanlibandua',
+        password: '',
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
     });
-    var dsloaisp = await pool.query('SELECT * from sanpham ');
-    Bangloaisp = dsloaisp[1];
-    var kq = "";
+    var dssptheogia;
+    if (khoanggia == 0){
+        dssptheogia = await pool.query('select  * from sanpham');
+    }   
+        else
+        dssptheogia = await pool.query("select  * from sanpham where KHOANGGIA=" + khoanggia);
+    Bangloaisp = dssptheogia[0];
+    var kq;
     for (i = 0; i < Bangloaisp.length; i++) {
-
-        kq += "<a href='/?maloai="
-            + Bangloaisp[i].Maloai + "'>" + Bangloaisp[i].Maloai + "</a><br>";
+        if (i % 6 == 0)
+            kq += "<ul class='nospace clear'>";
+            kq += "<li class='one_quarter'>"
+            kq += "<div class='card'>";
+            kq += "<img width= '200px' height='200px' src='images/" + Bangloaisp[i].HINH + "'/>"
+            kq += "<a href='/?sanpham=1&masp=" + Bangloaisp[i].MASP + "'> ";
+            kq += "<p class='title'>&nbsp;"+ Bangloaisp[i].TENSP +"</p></a>";
+            kq += "<div class='Gia'>&nbsp;Giá:" + Bangloaisp[i].DONGIA + " VND&nbsp;&nbsp;&nbsp"
+            kq += "<a href='/?muasp=" + Bangloaisp[i].MASP + "&tensp=";
+            kq += Bangloaisp[i].TENSP + "&dongia=" + Bangloaisp[i].DONGIA + "&hinh=" + Bangloaisp[i].HINH + "'>" + "<i class='fa fa-lg fa-cart-plus'></i></a></div>";
+            kq += "</div>"
+            kq += "</li>&nbsp;&nbsp;&nbsp;&nbsp";
+            if ((i + 1) % 6 == 0)
+            kq += "</ul>";
     }
-
     return kq;
-}
-
-module.exports.GioHangTong = function (giohang) {
-    var tongtien = 0;
-    var soluong = 0;
-    var kq=""
-    for (i = 0; i < giohang.length; i++) {
-
-        soluong += giohang[i].soluong*1;
-        tongtien += giohang[i].soluong * giohang[i].dongia;
-
-    }
-    kq += "<br>Số lượng : " + soluong;
-    kq += "<br>Tổng tiền :" + tongtien;
-    kq += "<br><a href='/?ttgh=1'>Thông tin giỏ hàng</a>";
-    return kq;
-
 };
-/*
+
 module.exports.HienThiGioHang = function (giohang) {
     var tongtien = 0;
-    var kq = "<table width='90%' border='1' cellspacing='0' ";
-    kq+="cellpadding = '0' > ";
-    kq += "<tr><td width='5%'>STT</td><td width='30%'>Tên hoa</td>";
-    kq += "<td width='10%'>SL</td> <td width='15%'>Giá</td>";
-    kq+="<td width='15%'>Thành tiền</td><td width='10%'>Xóa</td><td>Sửa</td></tr > ";
+    var kq;
     for (i = 0; i < giohang.length; i++) {
-        kq += "<tr><td>" + (i + 1) + "</td><td>" + giohang[i].tenhoa
-            + "</td>";
-        kq += "<td><input type='number' id='txtsl" + giohang[i].mahoa+"' value='" + giohang[i].soluong +"'/></td><td>" + giohang[i].dongia + "</td>";
-        kq += "<td>" + giohang[i].soluong * giohang[i].dongia
-            + "</td>";
-        kq += "<td><a href='#' onClick='xoahoa(" + giohang[i].mahoa+")'>Xóa</a></td>" ;
-        kq += "<td><a href='#' onClick='suahoa(" + giohang[i].mahoa +")'>Sửa</a></td></tr>";
-        tongtien += giohang[i].soluong * giohang[i].dongia;
-    }
-    kq += "<tr><td colspan='7' align='right'>Tổng tiền :" + tongtien + "</td></tr>";
-    kq += "</table";
-    return kq;
+    kq += "<div class='product'>"
+    kq += "<div class='product-image'>"
+    
+    kq += "  <img src='images/" + giohang[i].hinhsp +"'/>"
+    kq += "</div>"
+    kq += "<div class='product-details'>"
+    kq += "  <div class='product-title'>" + giohang[i].tensp +"</div>"
+    kq += "</div>"
+    kq += "<div class='product-price'> " + giohang[i].dongia +"vnd</div>"
+    kq += "<div class='product-quantity'> " + giohang[i].soluong +"</div>"
+    kq += "<div class='product-removal'>"
+    kq += "  <button class='remove-product'>"
+    kq += "    Xóa"
+    kq += "  </button>"
+    kq += "</div>"
+    kq += "<div class='product-line-price'>" + giohang[i].soluong * giohang[i].dongia +"vnd</div>"
+    kq += "</div>";
+    tongtien += giohang[i].soluong * giohang[i].dongia;
 
-};*/
+}
+kq += "<div class='totals'>"
+kq += "<div class='totals-item totals-item-total'>"
+kq += "<label>TỔNG CỘNG</label>"
+kq += "<div class='totals-value' id='cart-total'> " + tongtien + "vnd</div>"
+kq += "  </div>"
+kq += "  </div>"
+    return kq;
+};
 
 module.exports.HienThiSP = async function (maloai) {
     var mysql = require('mysql2/promise');
@@ -128,17 +134,56 @@ module.exports.HienThiSP = async function (maloai) {
     var kq;
     for (i = 0; i < Bangsp.length; i++) {
         if (i % 6 == 0)
-        kq += "<ul class='nospace clear'>";
+            kq += "<ul class='nospace clear'>";
             kq += "<li class='one_quarter'>"
             kq += "<div class='card'>";
             kq += "<img width= '200px' height='200px' src='images/" + Bangsp[i].HINH + "'/>"
             kq += "<a href='/?sanpham=1&masp=" + Bangsp[i].MASP + "'> ";
             kq += "<p class='title'>&nbsp;"+ Bangsp[i].TENSP +"</p></a>";
             kq += "<div class='Gia'>&nbsp;Giá:" + Bangsp[i].DONGIA + " VND&nbsp;&nbsp;&nbsp"
-            kq += "<a href='/?mua=1&masp=" + Bangsp[i].MASP +"'>" + "<i class='fa fa-lg fa-cart-plus'></i></a></div>";
+            kq += "<a href='/?muasp=" + Bangsp[i].MASP + "&tensp=";
+            kq += Bangsp[i].TENSP + "&dongia=" + Bangsp[i].DONGIA + "&hinh=" + Bangsp[i].HINH + "'>" + "<i class='fa fa-lg fa-cart-plus'></i></a></div>";
             kq += "</div>"
             kq += "</li>&nbsp;&nbsp;&nbsp;&nbsp";
-        if ((i + 1) % 6 == 0)
+            if ((i + 1) % 6 == 0)
+            kq += "</ul>";
+    }
+    return kq;
+};
+
+module.exports.HienThiSPTen = async function (tensp) {
+    var mysql = require('mysql2/promise');
+    var pool = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        database: 'quanlibandua',
+        password: '',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+    var dssp;
+    if (tensp == 0){
+        dssp = await pool.query('select  * from sanpham');
+    }   
+        else
+        dssp = await pool.query("select  * from sanpham where tensp like'%" + tensp + "%' or mota like '%" + tensp +"%'");
+    Bangsp = dssp[0];
+    var kq;
+    for (i = 0; i < Bangsp.length; i++) {
+        if (i % 6 == 0)
+            kq += "<ul class='nospace clear'>";
+            kq += "<li class='one_quarter'>"
+            kq += "<div class='card'>";
+            kq += "<img width= '200px' height='200px' src='images/" + Bangsp[i].HINH + "'/>"
+            kq += "<a href='/?sanpham=1&masp=" + Bangsp[i].MASP + "'> ";
+            kq += "<p class='title'>&nbsp;"+ Bangsp[i].TENSP +"</p></a>";
+            kq += "<div class='Gia'>&nbsp;Giá:" + Bangsp[i].DONGIA + " VND&nbsp;&nbsp;&nbsp"
+            kq += "<a href='/?muasp=" + Bangsp[i].MASP + "&tensp=";
+            kq += Bangsp[i].TENSP + "&dongia=" + Bangsp[i].DONGIA + "&hinh=" + Bangsp[i].HINH + "'>" + "<i class='fa fa-lg fa-cart-plus'></i></a></div>";
+            kq += "</div>"
+            kq += "</li>&nbsp;&nbsp;&nbsp;&nbsp";
+            if ((i + 1) % 6 == 0)
             kq += "</ul>";
     }
     return kq;
@@ -170,7 +215,8 @@ module.exports.ChiTietSP = async function (masp) {
         kq += "<p><strong>Số lượng: </strong></p>"
         kq += "<div class='product-quantity'>";
         kq +=  "<input type='number' value='1' min='1'></div>";
-        kq += "<p><a href='#' class='buy-now btn btn-sm height-auto px-4 py-3 btn-primary'><i class='fa fa-lg fa-cart-plus'></i></a></p>";   
+        kq += "<p><a href='/?muasp=" + spht[0].MASP + "&tensp=";
+        kq += spht[0].TENSP + "&dongia=" + spht[0].DONGIA + "&hinh=" + spht[0].HINH + "' class='buy-now btn btn-sm height-auto px-4 py-3 btn-primary'><i class='fa fa-lg fa-cart-plus'></i></a></p>";   
         kq += "<br></td>";
         kq+="</tr > ";
         kq += "</table>";
